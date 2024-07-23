c6b1b94986ddb260cd506aeb189badccf2858e88
Guilherme Freitas <guilherme.f@onerhino.com>
2024-07-23 19:37:08 -0300

    add: maximum subarray

---
+++ src/data-structure/array/maximum-subarray.ts
@@ -0,0 +1,14 @@
+// with the largest sum, and return its sum.
+
+
+function maxSubArray(nums: number[]): number {
+  let local = 0
+  let global = -Infinity
+
+  for (const num of nums) {
+    local = Math.max(num, local + num)
+    if (local > global) { global = local }
+  }
+
+  return global
+}
